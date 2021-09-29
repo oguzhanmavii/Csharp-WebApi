9dfd82d877fc24c2a930261023c50274b477329c
OĞUZHAN MAVİ <mavioguz09@gmail.com>
2021-09-30 01:21:52 +0300

    WEB_API.NETCORE
    WEB_API

---
+++ first_webapi_app/HotelFinder.Business/Abstract/IHotelService.cs
@@ -0,0 +1,23 @@
+﻿using HotelFinder.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelFinder.Business.Abstract
+{
+    public interface IHotelService
+    {
+        List<Hotel> GetAllHotel();
+
+        Hotel GetHotelById(int id);
+
+        Hotel CreateHotel(Hotel hotel);
+
+        Hotel updatedHotel(Hotel hotel);
+
+        void DeleteHotel(int id);
+
+    }
+}
---
+++ first_webapi_app/first_webapi_app/Startup.cs
@@ -0,0 +1,60 @@
+using HotelFinder.Business.Abstract;
+using HotelFinder.Business.Concrete;
+using HotelFinder.DataAccess.Abstract;
+using HotelFinder.DataAccess.Concrete;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace first_webapi_app
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();//Kontrol eklemek için yazýlacak kod 
+            services.AddSingleton<IHotelService, HotelManager>(); // HotelManager'ý çaðýrmak için yazýlacak kod
+            services.AddSingleton<IHotelRepository, HotelRepository>();// Reposityor'i çaðýrmak için yazýlan kod 
+
+          
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();                
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
